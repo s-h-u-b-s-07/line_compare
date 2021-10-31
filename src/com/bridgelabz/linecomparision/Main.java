2f7dadd2ecf9858a07066df2de0c980172bd07af
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-10-31 23:10:33 +0530

    welcome to program

---
+++ src/com/bridgelabz/linecomparision/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.linecomparision;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to line comparison computation program");
+    }
+}
