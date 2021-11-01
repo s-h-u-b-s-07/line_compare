1dc5c511f10a2e4145f915238d29222423e0e985
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-11-01 11:05:45 +0530

    line Equality checkup

--- src/com/bridgelabz/linecomparision/line_compare.java
+++ src/com/bridgelabz/linecomparision/line_compare.java
@@ -8,7 +8,7 @@ public class line_compare {
 
         int x1, x2, y1, y2;
 
-        //taking user inputs for co-ordinates of line 1
+        //taking the user inputs for co-ordinates of line 1
         Scanner input = new Scanner(System.in);
 
         System.out.println("Enter coordinate of line-1\n" +
@@ -21,13 +21,13 @@ public class line_compare {
         System.out.println("Enter y2 point");
         y2 = input.nextInt();
 
-        //computing length of line 1
+        //computing the length of line 1
         double length1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 
         //taking variables for co-ordinates of line 2
         int x3, x4, y3, y4;
 
-        //taking user inputs for co-ordinates of line 2
+        //taking the user inputs for co-ordinates of line 2
         Scanner input2 = new Scanner(System.in);
 
         System.out.println("Enter coordinate of line-2\n" +
@@ -40,19 +40,24 @@ public class line_compare {
         System.out.println("Enter y4 point");
         y4 = input2.nextInt();
 
-        //computing length of line 2
+        //computing the length of line 2
         double length2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
 
-        //printing the lengths
+        //printing the lengths of lines
         System.out.println("Length of line-1 " + "(" + x1 + "," + y1 + "),(" + x2 + "," + y2 + ") = " + length1);
         System.out.println("Length of line-2 " + "(" + x3 + "," + y3 + "),(" + x4 + "," + y4 + ") = " + length2);
 
         //comparing the lengths of lines
         if (length1 == length2) {
-            System.out.println("The given lines are eual");
-        } else {
-            System.out.println("The given lines are not equal");
+            System.out.println("Line-1 and Line-2 are equal");
         }
+        else if (length1 < length2) {
+            System.out.println("Line-1 is less than Line-2");
+        }
+        else if (length1 > length2) {
+            System.out.println("Line-1 is greater than Line-2");
+        }
+
 
 
     }
